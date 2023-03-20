2caaa89f9fdb6929548145b2e6fe4fe2289316c5
JeFeel <jaephil94@gmail.com>
2023-03-21 02:14:47 +0900

    주석 추가

--- 3.객체/객체 연습과제.js
+++ 3.객체/객체 연습과제.js
@@ -1,6 +1,6 @@
 var user = {};
 user.name = 'John';
-user.surname = 'Smith';
-user.name = 'Pete';
-delete user.name;
+user.surname = 'Smith'; //{ name: 'John', surname: 'Smith' }
+user.name = 'Pete';   //{ name: 'Pete', surname: 'Smith' }
+delete user.name;  //{ surname: 'Smith' }
 console.log(user);
