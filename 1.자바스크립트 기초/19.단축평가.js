a19433c7e3ecc34f423538fca8e21a4f96d9b608
JeFeel <jaephil94@gmail.com>
2023-03-21 18:47:13 +0900

    주석 추가

--- 1.자바스크립트 기초/19.단축평가.js
+++ 1.자바스크립트 기초/19.단축평가.js
@@ -1,13 +1,13 @@
 
 
 // OR 연산: 첫번째 truthy를 반환
-console.log('hello' || 'world'); //hello ???
+console.log('hello' || 'world'); //둘 다 true라서 왼쪽부터 선택됨
 console.log(0 || '안녕');   //안녕
 
 // AND 연산: 첫번째 falsy를 반환
 console.log('=================');
 
-console.log('HELLO' && 'WORLD');    //WORLD ???
+console.log('HELLO' && 'WORLD');    //둘 다 true라서 오른쪽 끝까지 확인해도 false 없으니 WORLD
 console.log(null && '메롱');        //null
 
 /*
