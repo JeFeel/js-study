0e381d12f05b0c489bd9ac5c3ab5ce173cd77a38
JeFeel <jaephil94@gmail.com>
2023-03-21 12:49:03 +0900

    const 사용법

---
+++ 4.함수/11.const.js
@@ -0,0 +1,27 @@
+
+const TODAY_SALE_RATE = 0.2;
+
+//const: '상수' 값의 변경이 불가함
+
+const PI = 3.14159265;
+
+//PI = 5.5; //TypeError: Assignment to constant variable.
+
+const kim ={
+  name: '김철수',
+  age: 25
+};
+
+kim.name = '김칠리';
+
+// kim ={
+//   name: '김철수',
+//   age: 25
+// }; //TypeError: Assignment to constant variable.
+
+const a = 10;
+const b = 20;
+
+for(let i=0;i<5;i++){
+  console.log(i);
+}
